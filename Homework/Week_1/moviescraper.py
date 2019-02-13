8c52e84b32793296740af4a64a4822429640cf99
Marije Brandsma <MarijeRoelofje@gmail.com>
2019-02-14 00:12:04 +0100

    Worked on assignment

--- Homework/Week_1/moviescraper.py
+++ Homework/Week_1/moviescraper.py
@@ -19,58 +19,47 @@ OUTPUT_CSV = 'movies.csv'
 def extract_movies(dom):
     """
     Extract a list of highest rated movies from DOM (of IMDB page).
-    Each movie entry should contain the following fields:
+    Each movie entry contains the following fields:
     - Title
     - Rating
-    - Year of release (only a number!)
-    - Actors/actresses (comma separated if more than one)
-    - Runtime (only a number!)
+    - Year of release
+    - Runtime
     """
 
-    # ADD YOUR CODE HERE TO EXTRACT THE ABOVE INFORMATION ABOUT THE
-    # HIGHEST RATED MOVIES
-    # NOTE: FOR THIS EXERCISE YOU ARE ALLOWED (BUT NOT REQUIRED) TO IGNORE
-    # UNICODE CHARACTERS AND SIMPLY LEAVE THEM OUT OF THE OUTPUT.
-
-    # Parse the movie list from imdb with BeautifulSoup
+    # Parses the movie list from IMDB with BeautifulSoup
     imdb_soup = BeautifulSoup(html, "lxml")
     list_soup = imdb_soup.find_all('div', {'class':'lister-item-content'})
 
-    # Create empty movie list
+    # Creates an empty movie list
     movie_list = []
 
-    # loop through every movie in the list
+    # Loops through every movie in the list
     for movies in list_soup:
 
-        # Parse the title of the movie
+        # Parses the title of the movie
         movie_title = movies.find('h3')
         movie_title = movie_title.a.string
 
-        # Parse the rating of the movie
+        # Parses the rating of the movie
         movie_rating = movies.find('strong')
         movie_rating = float(movie_rating.string)
 
-        # Parse the year of release of the movie
+        # Parses the year of release of the movie
         movie_year = movies.find('span', {'class':'lister-item-year text-muted unbold'})
         movie_year = movie_year.string
         movie_year = ''.join(c for c in movie_year if c not in '()I ')
 
-        # Parse the actors of the movie
-        #movie_staff = movies.find('p', {'class':''})
-        #print("Staff:")
-        #print(movie_staff)
-
-        # Parse the runtime of the movie
+        # Parses the runtime of the movie
         movie_runtime = movies.find('span', {'class':'runtime'})
         movie_runtime = movie_runtime.string
         movie_runtime = ''.join(c for c in movie_runtime if c not in ' min')
 
-        # Adds all the parsed information to a movie dict and adds the movie to movie_list
+        # Adds all the parsed information to a movie dict and adds the movie to
+        # movie_list
         movie_details = dict()
         movie_details['title'] = movie_title
         movie_details['rating'] = movie_rating
         movie_details['year'] = movie_year
-        #movie_details['actors'] = movie_actors 
         movie_details['runtime'] = movie_runtime
         movie_list.append(movie_details)
 
@@ -83,11 +72,11 @@ def save_csv(outfile, movies):
     Output a CSV file containing highest rated movies.
     """
     writer = csv.writer(outfile)
-    writer.writerow(['Title', 'Rating', 'Year', 'Actors', 'Runtime'])
+    writer.writerow(['Title', 'Rating', 'Year', 'Runtime'])
 
     # Writes the movies to the disk
     for movie in movies:
-        writer.writerow([movie['title'], movie['rating'], movie['year'], 'None', movie['runtime']])
+        writer.writerow([movie['title'], movie['rating'], movie['year'], movie['runtime']])
 
 
 def simple_get(url):
@@ -119,21 +108,21 @@ def is_good_response(resp):
 
 if __name__ == "__main__":
 
-    # get HTML content at target URL
+    # Gets HTML content at target URL
     html = simple_get(TARGET_URL)
 
-    # save a copy to disk in the current directory, this serves as an backup
+    # Saves a copy to disk in the current directory, this serves as an backup
     # of the original HTML, will be used in grading.
     with open(BACKUP_HTML, 'wb') as f:
         f.write(html)
 
-    # parse the HTML file into a DOM representation
+    # Parses the HTML file into a DOM representation
     dom = BeautifulSoup(html, 'html.parser')
 
-    # extract the movies (using the function you implemented)
+    # Extracts the movies (using the function you implemented)
     movies = extract_movies(dom)
     movies = movies[0]
 
-    # write the CSV file to disk (including a header)
+    # Writes the CSV file to disk (including a header)
     with open(OUTPUT_CSV, 'w', newline='') as output_file:
         save_csv(output_file, movies)
--- Homework/Week_1/visualizer.py
+++ Homework/Week_1/visualizer.py
@@ -13,33 +13,31 @@ INPUT_CSV = "movies.csv"
 START_YEAR = 2008
 END_YEAR = 2018
 
-# Create an empty movie list
+# Creates an empty movie list
 movie_list = []
 
-# Store movies from csv file in a dictionary
+# Stores movies from csv file in a dictionary
 with open(INPUT_CSV, newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        movie = (row['Title'], row['Rating'], row['Year'], row['Actors'], row['Runtime'])
+        movie = (row['Title'], row['Rating'], row['Year'], row['Runtime'])
         movie_list.append(movie)
 
 # Global dictionary for the data
 data_dict = {str(key): [] for key in range(START_YEAR, END_YEAR)}
 
-# Add movie ratings to the list of the correct year
+# Adds movie ratings to the list of the correct year
 for item in movie_list:
     movie_rating = float(item[1])
     movie_year = item[2]
-
-    # Add movie rating to correct year
     data_dict[movie_year].append(movie_rating)
 
-# Calculate averages for every year
+# Calculates averages for every year
 avg_movie_ratings = {}
-for year,rating in data_dict.items():
+for year, rating in data_dict.items():
     avg_movie_ratings[year] = sum(rating)/ len(rating)
 
-# Create a plot that shows average ratings of 2008-2017
+# Creates a plot that shows average ratings of 2008-2017
 plt.plot(range(len(avg_movie_ratings)), list(avg_movie_ratings.values()))
 plt.xticks(range(len(avg_movie_ratings)), list(avg_movie_ratings.keys()))
 plt.ylabel('Average rating')

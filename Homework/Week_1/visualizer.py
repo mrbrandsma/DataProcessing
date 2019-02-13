611d6cd379c44b34485f56b0cc2c9a7ed3b688df
Marije Brandsma <MarijeRoelofje@gmail.com>
2019-02-13 23:47:22 +0100

    Added assignment "Scraping"

---
+++ Homework/Week_1/visualizer.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# Name: Marije Brandsma
+# Student number: 11389257
+"""
+This script visualizes data obtained from a .csv file
+"""
+
+import csv
+import matplotlib.pyplot as plt
+
+# Global constants for the input file, first and last year
+INPUT_CSV = "movies.csv"
+START_YEAR = 2008
+END_YEAR = 2018
+
+# Create an empty movie list
+movie_list = []
+
+# Store movies from csv file in a dictionary
+with open(INPUT_CSV, newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        movie = (row['Title'], row['Rating'], row['Year'], row['Actors'], row['Runtime'])
+        movie_list.append(movie)
+
+# Global dictionary for the data
+data_dict = {str(key): [] for key in range(START_YEAR, END_YEAR)}
+
+# Add movie ratings to the list of the correct year
+for item in movie_list:
+    movie_rating = float(item[1])
+    movie_year = item[2]
+
+    # Add movie rating to correct year
+    data_dict[movie_year].append(movie_rating)
+
+# Calculate averages for every year
+avg_movie_ratings = {}
+for year,rating in data_dict.items():
+    avg_movie_ratings[year] = sum(rating)/ len(rating)
+
+# Create a plot that shows average ratings of 2008-2017
+plt.plot(range(len(avg_movie_ratings)), list(avg_movie_ratings.values()))
+plt.xticks(range(len(avg_movie_ratings)), list(avg_movie_ratings.keys()))
+plt.ylabel('Average rating')
+plt.xlabel('Year')
+plt.show()
+
+if __name__ == "__main__":
+    print(data_dict)
